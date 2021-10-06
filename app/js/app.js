dd946d2c6dd5b76d023365986670667c03ba5faf
kirillgileadd <kirillgilead@gmail.com>
2021-10-07 02:47:36 +0300

    v1.3

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,11 @@
 // // Import vendor jQuery plugin example
 import Swiper from 'swiper/bundle'
+import videojs from 'video.js'
 // import '~/app/libs/mmenu/dist/mmenu.js'
 const swiper = new Swiper('.swiper', {
 	// Optional parameters
 	loop: true,
+	slidesPerView: 5,
 
 	// If we need pagination
 	pagination: {
@@ -19,6 +21,9 @@ const swiper = new Swiper('.swiper', {
 	// And if we need scrollbar
 });
 document.addEventListener('DOMContentLoaded', () => {
-
+	// videojs(document.querySelector('#video-1'))
+	// videojs(document.querySelector('#video-2'))
+	// videojs(document.querySelector('#video-3'))
+	// videojs(document.querySelector('#video-4'))
 
 })
