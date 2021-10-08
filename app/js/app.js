94d9f703dcc31131aabe8889f8d561d145f01ae9
kirillgileadd <kirillgilead@gmail.com>
2021-10-08 19:07:41 +0300

    v1.4

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,7 @@
 // // Import vendor jQuery plugin example
 import Swiper from 'swiper/bundle'
 import videojs from 'video.js'
+import noUiSlider from 'nouislider'
 // import '~/app/libs/mmenu/dist/mmenu.js'
 const swiper = new Swiper('.swiper', {
 	// Optional parameters
@@ -20,10 +21,46 @@ const swiper = new Swiper('.swiper', {
 
 	// And if we need scrollbar
 });
-document.addEventListener('DOMContentLoaded', () => {
-	// videojs(document.querySelector('#video-1'))
-	// videojs(document.querySelector('#video-2'))
-	// videojs(document.querySelector('#video-3'))
-	// videojs(document.querySelector('#video-4'))
+videojs(document.querySelector('#video-1', {
+	controlBar: {
+		fullscreenToggle: false
+	},
+}))
+videojs(document.querySelector('#video-2'))
+videojs(document.querySelector('#video-3'))
+videojs(document.querySelector('#video-4'))
 
+document.addEventListener('DOMContentLoaded', () => {
+
+
+	document.addEventListener('click', function (e) {
+		var map = document.querySelector('#map-wrap iframe')
+		if (e.target.id === 'map-wrap') {
+			map.style.pointerEvents = 'all'
+		} else {
+			map.style.pointerEvents = 'none'
+		}
+	})
+	// Price 
+
+	var slider = document.getElementById('slider');
+
+
+	noUiSlider.create(slider, {
+		range: {
+			'min': 0,
+			'10%': 10,
+			'23%': 23,
+			'30%': 30,
+			// Nope, 40 is no fun.
+			'50%': 50,
+			'60%': 60,
+			'70%': 70,
+			// I never liked 80.
+			'90%': 90,
+			'max': 100
+		},
+		snap: true,
+		start: [0]
+	});
 })
