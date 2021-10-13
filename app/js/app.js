49bd1ef0ec38735b3756816f89939807b3a1cebc
kirillgileadd <kirillgilead@gmail.com>
2021-10-13 20:09:49 +0300

    v1.5

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,8 @@
 import Swiper from 'swiper/bundle'
 import videojs from 'video.js'
 import noUiSlider from 'nouislider'
+import IMask from 'imask';
+import mixitup from 'mixitup';
 // import '~/app/libs/mmenu/dist/mmenu.js'
 const swiper = new Swiper('.swiper', {
 	// Optional parameters
@@ -21,17 +23,31 @@ const swiper = new Swiper('.swiper', {
 
 	// And if we need scrollbar
 });
-videojs(document.querySelector('#video-1', {
-	controlBar: {
-		fullscreenToggle: false
-	},
-}))
+
+let video1 = document.querySelector('#video-1')
+video1.volume = 0.1
+videojs(video1)
 videojs(document.querySelector('#video-2'))
 videojs(document.querySelector('#video-3'))
 videojs(document.querySelector('#video-4'))
 
 document.addEventListener('DOMContentLoaded', () => {
 
+	/// INPUT 
+
+	var elements = document.querySelectorAll('#phone');
+	var maskOptions = {
+		mask: '+7(000)000-00-00',
+		lazy: false
+	}
+
+	if (elements) {
+		for (let i = 0; i < elements.length; i++) {
+			let mask = new IMask(elements[i], maskOptions)
+		}
+	}
+
+	// MAP 
 
 	document.addEventListener('click', function (e) {
 		var map = document.querySelector('#map-wrap iframe')
@@ -43,24 +59,35 @@ document.addEventListener('DOMContentLoaded', () => {
 	})
 	// Price 
 
-	var slider = document.getElementById('slider');
+	const R1315 = [120, 125, 105, 30, 50, 200, 100, 350, 300 - 600, 500 - 700, 600 - 1400, 400, 1.2, 500 / 800, 500 / 800,]
+	const R16 = [220, 225, 205, 50, 70, 300, 200, 450, 300 - 800, 600 - 800, 700 - 1400, 400, 1.2, 500 / 800, 500 / 800,]
+	const R17 = [320, 325, 305, 60, 80, 400, 100, 350, 300 - 600, 500 - 700, 600 - 1400, 400, 1.2, 500 / 800, 500 / 800,]
+	const R18 = [420, 325, 405, 70, 90, 500, 100, 350, 300 - 600, 500 - 700, 600 - 1400, 400, 1.2, 500 / 800, 500 / 800,]
+	const R19 = [520, 425, 505, 80, 100, 600, 100, 350, 300 - 600, 500 - 700, 600 - 1400, 400, 1.2, 500 / 800, 500 / 800,]
+	const R20 = [620, 525, 605, 90, 110, 700, 100, 350, 300 - 600, 500 - 700, 600 - 1400, 400, 1.2, 500 / 800, 500 / 800,]
+	const R2122 = [620, 625, 705, 100, 120, 800, 100, 350, 300 - 600, 500 - 700, 600 - 1400, 400, 1.2, 500 / 800, 500 / 800,]
 
+	const sliderItems = document.querySelectorAll('.slider__item')
+	if (sliderItems) {
+		sliderItems.forEach((sliderItem) => {
+			sliderItem.addEventListener('click', () => {
+				removeAllClasses()
+				sliderItem.classList.add('active')
+			})
+		})
+	}
 
-	noUiSlider.create(slider, {
-		range: {
-			'min': 0,
-			'10%': 10,
-			'23%': 23,
-			'30%': 30,
-			// Nope, 40 is no fun.
-			'50%': 50,
-			'60%': 60,
-			'70%': 70,
-			// I never liked 80.
-			'90%': 90,
-			'max': 100
-		},
-		snap: true,
-		start: [0]
+	function removeAllClasses() {
+		sliderItems.forEach((sliderItem) => {
+			sliderItem.classList.remove('active')
+		})
+	}
+
+	var mixer = mixitup('.price__inner', {
+		load: {
+			filter: '.category-a'
+		}
 	});
+
+
 })
